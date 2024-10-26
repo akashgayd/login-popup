c0037817f8868ece5b79960159886d1326777adf
akash gaydhane <akashgaydhane781@gmail.com>
2024-10-26 18:42:29 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let loginEl = document.getElementById("login");
+
+
+let sigup = document.getElementById("sigup");
+
+
+function onLogin(){
+
+    loginEl.classList.add("on-login")
+}
+
+
+function removelLogin(){
+
+    loginEl.classList.remove("on-login")
+
+}
+
+
+
+function onsigup(){
+
+    sigup.classList.add("on-sigup")
+}
+
+
+function removelSigup(){
+
+    sigup.classList.remove("on-sigup")
+
+}
